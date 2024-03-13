e513667f40f6d065e5bab532aafaffbd9801b095
Svyatoslav Petrov <sp.gro.33@gmail.com>
2024-03-13 21:11:23 +0300

    Add task

---
+++ HW15.6.6/HW15.6.6/Classroom.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HW15_6_6;
+
+namespace HW15_6_6
+{
+    internal class Classroom
+    {
+        public List<string> Students = new List<string>();
+    }
+}
---
+++ HW15.6.6/HW15.6.6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using HW15_6_6;
+
+namespace HW15_6_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var classes = new[]
+            {
+               new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
+               new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
+               new Classroom { Students = {"Bulat", "Alex", "Galina"}, }
+           };
+            var allStudents = GetAllStudents(classes);
+
+            Console.WriteLine(string.Join(" ", allStudents));
+        }
+        static string[] GetAllStudents(Classroom[] classes)
+        {
+            var allStudents = from c in classes
+                              from students in c.Students
+                              select students;
+            return allStudents.ToArray();
+        }
+    }
+}
